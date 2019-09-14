0747ec6313c950f7de0bda3bc48a6f016ae14937
moonmingyu <54879647+moonmingyu@users.noreply.github.com>
2019-09-14 13:23:56 +0900

    Add files via upload

---
+++ aa06_rpt02/aa06info.js
@@ -0,0 +1,19 @@
+//aa06info.js
+module.exports.myinfo = function(id, name, phone) {
+	console.log("My Info");
+	console.log("ID : " + id);
+	console.log("Name : " + name);
+	console.log("Phone : " + phone +"\n");
+}
+module.exports.suck = function(id, name, phone) {
+	console.log("jinsuck info");
+	console.log("ID : " + id);
+	console.log("Name : " + name);
+	console.log("Phone : " + phone +"\n");
+}
+module.exports.lee = function(id, name, phone) {
+	console.log("Mr.Lee Info");
+	console.log("ID : " + id);
+	console.log("Name : " + name);
+	console.log("Phone : " + phone +"\n");
+}
---
+++ aa06_rpt02/index_aa06.js
@@ -0,0 +1,5 @@
+//index_aa06.js
+var info = require('./aa06info');
+info.myinfo("aa06", "moon", "010-1222-3456");
+info.suck("aa07", "park", "010-2222-3333");
+info.lee("aa08", "lee", "010-7890-1234");
